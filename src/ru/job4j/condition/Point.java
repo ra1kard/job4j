faeb4783018fd1f322d6c5b0acdcd03537e2fde2
Stanislav Kufarev <91raykard@gmail.com>
2019-07-15 23:43:51 +0300

    works at a home

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -7,7 +7,9 @@ public class Point {
     }
 
     public static void main(String[] args) {
-        double result = distance(1, 1,5,3);
+        double result = distance(0, 0,2,0);
         System.out.println("result (0, 0) to (2, 0) " + result);
+        double result2 = distance(1, 1,5,3);
+        System.out.println("result (1, 1) to (5, 3) " + result2);
     }
 }
