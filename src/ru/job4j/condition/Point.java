1f478405fd360c5bbcd4535c6e2aa38e816c2b5c
Stanislav Kufarev <91raykard@gmail.com>
2019-07-15 23:37:15 +0300

    taskPoint#159313

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -2,14 +2,12 @@ package ru.job4j.condition;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2){
-        int x = x2 - x1;
-        int y = y2 - y1;
-        double first = Math.sqrt((Math.pow(x,2) + (Math.pow(y,2))));
+        double first = Math.sqrt(Math.pow((x2-x1),2) + (Math.pow((y2-y1),2)));
         return first;
     }
 
     public static void main(String[] args) {
-        double result = distance(0, 0,2,0);
+        double result = distance(1, 1,5,3);
         System.out.println("result (0, 0) to (2, 0) " + result);
     }
 }
