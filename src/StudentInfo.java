71d722af6f6db9b2d5fb839571b47d7c0e0412e7
Stanislav Kufarev <91raykard@gmail.com>
2019-07-18 12:44:32 +0300

    1.2. Подключить сборщик Maven[#4086]

--- src/StudentInfo.java
+++
@@ -1,7 +0,0 @@
-public class StudentInfo {
-
-    public static void main(String[] args) {
-        System.out.println("Stanislav Kufarev");
-        System.out.println("05.11.1991");
-    }
-}
--- src/ru/job4j/Main.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
