a510cddba24df95a6df88ff9734591915daa20fe
Stanislav Kufarev <91raykard@gmail.com>
2019-07-19 11:26:52 +0300

    refactoring

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -2,6 +2,7 @@ package ru.job4j.calculate;
 
 public class Calculate {
     public static void main(String[] args) {
-        System.out.println("Hello, Job4j.");
+        System.out.println("Hello, Job4j."
+        );
     }
 }
