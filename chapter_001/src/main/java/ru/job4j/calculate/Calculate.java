b66a450085c7e32a4bfa4c8ec34242067be52bdb
Stanislav Kufarev <91raykard@gmail.com>
2019-07-20 15:42:39 +0300

    edit Job4j -> job4j

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -2,7 +2,6 @@ package ru.job4j.calculate;
 
 public class Calculate {
     public static void main(String[] args) {
-        System.out.println("Hello, Job4j."
-        );
+        System.out.println("Hello, job4j.");
     }
 }
