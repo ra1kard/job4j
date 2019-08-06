69c1a7f420a7a4d6bc4b9f336b787de4e8ce66f5
Stanislav Kufarev <91raykard@gmail.com>
2019-08-06 09:06:58 +0300

    5.6. Ипотека.[#159291]

--- chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -12,15 +12,17 @@ public class Mortgage {
     public int year(int amount, int monthly, double percent) {  //100р   10р в мес   50% в год
         int year = 0;
         //произвести вычисление кредитной суммы за год и оставшейся
-        double creditInYear = amount + percent;                 //150р = 100р + 50%         кредит в год
-        double payInYear = monthly * 12;                        //120р = 10 * 12            платеж в год по факту
+        double remainder = 1;
 
-        while (payInYear < creditInYear) {                      //120 < 150
-            year++;                                             //+1
-            payInYear = creditInYear - payInYear;               //остаток платеж в год = 30р (150-120)
-            
+
+        while (remainder > 0) {
+            remainder = (amount + percent) - (monthly * 12);        //30р = 150р - 120р         остаток
+
+            //creditInYear = amount + percent;             //150р = 100р + 50%         кредит в год
+            //payInYear = monthly * 12;                    //120р = 10 * 12            платеж в год по факту
+
+            year++;
         }
-
         return year;
     }
 }
