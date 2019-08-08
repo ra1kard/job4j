d1e74a26f6ae659223ab007d6b912b5444788fd0
Stanislav Kufarev <91raykard@gmail.com>
2019-08-08 20:19:05 +0300

    PhraseMatic add

---
+++ head_first_java/src/main/java/com/oreilly/chapter_1/Exercise1b.java
@@ -0,0 +1,15 @@
+package com.oreilly.chapter_1;
+
+public class Exercise1b {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x < 10) {
+            if (x > 3) {
+                System.out.println("большой икс");
+            }
+            else {
+                x = x + 1;
+            }
+        }
+    }
+}
